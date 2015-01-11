e6b384199ef7fa3ab4f32a2142c9252b2f0da969
Francesco Anselmo <francesco.anselmo@arup.com>
2015-01-11 13:21:12 +0000

    Added AUTO script and new falsecolor and webcam site functions

---
+++ raspicamera_AUTO.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# Raspberry Pi camera HDR automatic capture
+# 2015-01-11 Francesco Anselmo
+# francesco.anselmo@gmail.com
+# francesco.anselmo@arup.com
+
+min_free_space = 100 # minimum free space in MB
+cmd_fmt = "raspistill -v -t 1 -vf -hf -w 1024 -h 768" # raspistill command format
+folder = "/opt/camera"
+link_to_webserver = True
+
+import ctypes
+import os
+import platform
+import sys
+import time
+import picamera
+
+def get_free_space_mb(folder):
+    """ Return folder/drive free space (in Mega bytes)
+    """
+    if platform.system() == 'Windows':
+        free_bytes = ctypes.c_ulonglong(0)
+        ctypes.windll.kernel32.GetDiskFreeSpaceExW(ctypes.c_wchar_p(folder), None, None, ctypes.pointer(free_bytes))
+        return free_bytes.value/1024/1024
+    else:
+        st = os.statvfs(folder)
+        return st.f_bavail * st.f_frsize/1024/1024
+
+def main():
+    free_space = get_free_space_mb(os.getcwd())
+    print("Free space: " + str(free_space) + " MB.")
+    if free_space > min_free_space:
+        date_time = time.strftime("%Y-%m-%d_%H%M")
+        print(date_time)
+        auto_photo_name = os.path.join(folder,"AUTO_"+date_time+".jpg")
+        # Automatic photo command
+	cmds = [cmd_fmt + " -o " + auto_photo_name ]
+        if link_to_webserver:
+            if platform.system() != 'Windows':
+                cmds.append("rm -f /usr/share/nginx/www/img/current.jpg")
+                cmds.append("ln -s %s %s" % (auto_photo_name, os.path.join("/usr/share/nginx/www/img","AUTO"+date_time+".jpg")))
+                cmds.append("ln -s %s %s" % (auto_photo_name, "/usr/share/nginx/www/img/current.jpg"))
+        # Print and execute commands
+        for cmd in cmds:
+            print(cmd)
+            os.system(cmd)
+    else:
+        print("The minimum available storage space needed is %s MB." % free_space) 
+        print("The system does not have enough storage space (%s MB). Stopping now." % min_free_space)
+
+if __name__ == "__main__":
+    main()
--- raspicamera_HDR.py
+++ raspicamera_HDR.py
@@ -13,6 +13,8 @@ speeds = [1000000/8000,1000000/4000,1000000/2000,1000000/1000,1000000/500,
 cmd_fmt = "raspistill -v -t 1 -vf -hf -w 1024 -h 768" # raspistill command format
 opt_man = "-awb none -ifx none -drc off" # raspistill manual options format
 folder = "/opt/camera"
+remove_previous_ldr = True
+link_to_webserver = True
 
 import ctypes
 import os
@@ -41,6 +43,18 @@ def main():
         auto_photo_name = os.path.join(folder,"AUTO_"+date_time+".jpg")
         # Automatic photo command
 	cmds = [cmd_fmt + " -o " + auto_photo_name ]
+        if link_to_webserver:
+            if platform.system() != 'Windows':
+                cmds.append("rm -f /usr/share/nginx/www/img/current.jpg")
+                cmds.append("ln -s %s %s" % (auto_photo_name, os.path.join("/usr/share/nginx/www/img","AUTO"+date_time+".jpg")))
+                cmds.append("ln -s %s %s" % (auto_photo_name, "/usr/share/nginx/www/img/current.jpg"))
+        # Remove previous LDR images to save space if requested
+        if remove_previous_ldr:
+            LDR_files = os.path.join(folder,"LDR*.*")
+            if platform.system() == 'Windows':
+                cmds.append("delete %s" % LDR_files)
+            else:
+                cmds.append("rm -f %s" % LDR_files)
         # LDR bracketed photo sequence command
         for ISO in ISOs:
             for speed in speeds:
@@ -55,10 +69,14 @@ def main():
         cmds.append("luminance-hdr-cli -v -s %s -o %s -t mantiuk08 %s" %(hdr_name, tm_name, os.path.join(folder,"LDR_"+date_time+"_*.jpg")))
         # Falsecolour commands
         for maxlum in (1,10,100,1000,10000):
-           fc_hdr_name = os.path.join(folder,"FC_"+date_time+"_"+str(maxlum).zfill(5)+".hdr")
-           fc_ldr_name = os.path.join(folder,"FC_"+date_time+"_"+str(maxlum).zfill(5)+".tif")
-           cmds.append("falsecolor -i %s -l cd/m2 -n 10 -s %s > %s" % (hdr_name,maxlum,fc_hdr_name))
-           cmds.append("ra_tiff -z %s %s" % (fc_hdr_name, fc_ldr_name))
+            fc_hdr_name = os.path.join(folder,"FC_"+date_time+"_"+str(maxlum).zfill(5)+".hdr")
+            fc_ldr_name = os.path.join(folder,"FC_"+date_time+"_"+str(maxlum).zfill(5)+".tif")
+            cmds.append("falsecolor -i %s -l cd/m2 -n 10 -s %s > %s" % (hdr_name,maxlum,fc_hdr_name))
+            cmds.append("ra_tiff -z %s %s" % (fc_hdr_name, fc_ldr_name))
+            if platform.system() == 'Windows':
+                cmds.append("delete %s" % fc_hdr_name)
+            else:
+                cmds.append("rm -f %s" % fc_hdr_name)
         # Print and execute commands
         for cmd in cmds:
             print(cmd)
